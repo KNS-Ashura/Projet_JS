efa2c950e2378917b24b261d4d43cf7b6f52ed0f
KNS-Ashura <160181900+KNS-Ashura@users.noreply.github.com>
2025-01-20 16:31:03 +0100

    Ajout de .gitignore et suppression de hide_key.js du suivi 2ieme essai

--- hide_key.js
+++
@@ -1,5 +0,0 @@
-export function generateMovieUrl(title) {
-    const apiKey = 'fe05fa529a04b25c74b7594d3251d327';
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(title)}`;
-    return url;
-}
